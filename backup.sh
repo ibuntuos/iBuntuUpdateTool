19c462c4f97c6b704cda76f1db17536b89c6b4de
ibuntuos <68012153+ibuntuos@users.noreply.github.com>
2020-07-12 12:10:39 +0200

    Version 1.0 last
    Now

--- backup.sh
+++ backup.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 BASEDIR=$(dirname "$0")
 if [ ! -d "$BASEDIR/Backup" ]; then
-#if [ ! -d $BASEDIR/Backup]; then
+echo "We try now to backup your packages, sourcelists and snaps"
+
 sudo mkdir $BASEDIR/Backup
 sudo mkdir $BASEDIR/Backup/Snaps
 sudo mkdir $BASEDIR/Backup/Applications
 sudo mkdir $BASEDIR/Backup/sources
 
 sudo cp -R /var/lib/snapd/*  $BASEDIR/Backup/Snaps/
+echo "sudo cp -R /usr/share/applications/*.desktop  ./Backup/Applications/"
 sudo cp -R /usr/share/applications/*.desktop  $BASEDIR/Backup/Applications/
+echo "sudo cp -R /etc/apt/sources.list.d/*  .R/Backup/sources/"
 sudo cp -R /etc/apt/sources.list.d/*  $BASEDIR/Backup/sources/
+echo "--------------------------------------------------------------------"
+echo "Backup done. This window should close automatically"
 fi
--- restore.sh
+++ restore.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 BASEDIR=$(dirname "$0")
+
 if [ -d "$BASEDIR/Backup" ]; then
+echo "We try now to restore your previously saved programs and settings"
+echo "sudo cp -R ./Backup/Snaps/* /var/lib/snapd/"
 sudo cp -R $BASEDIR/Backup/Snaps/* /var/lib/snapd/
+echo "sudo cp -R ./Backup/Applications/* /usr/share/applications/"
 sudo cp -R $BASEDIR/Backup/Applications/* /usr/share/applications/
+echo "sudo cp -R ./Backup/sources/* /etc/apt/sources.list.d/"
 sudo cp -R $BASEDIR/Backup/sources/* /etc/apt/sources.list.d/
+echo "-----------------------------------------------------------------"
+echo "Updating Package-Sources:"
 sudo apt update
+echo "-----------------------------------------------------------------"
+echo "Reinstall Packages:"
 xargs -a $BASEDIR/packages.list.save sudo apt install -y
+echo "-----------------------------------------------------------------"
+echo "Remove Backup:"
 sudo rm -R $BASEDIR/Backup
 fi

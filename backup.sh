b43406799286ee97a84d155a90f1b91e7ca95087
ibuntuos <68012153+ibuntuos@users.noreply.github.com>
2020-07-13 16:01:17 +0200

    Version 1.0
    Fix store

--- backup.sh
+++ backup.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 BASEDIR=$(dirname "$0")
 if [ ! -d "$BASEDIR/Backup" ]; then
-echo "We try now to backup your packages, sourcelists and snaps"
-
 sudo mkdir $BASEDIR/Backup
 sudo mkdir $BASEDIR/Backup/Snaps
 sudo mkdir $BASEDIR/Backup/Applications
 sudo mkdir $BASEDIR/Backup/sources
-
+fi
+sudo rm -R $BASEDIR/Backup/Snaps/*
+sudo rm -R $BASEDIR/Backup/Applicationsps/*
+sudo rm -R $BASEDIR/Backup/sources/*
+echo "We try now to backup your packages, sourcelists and snaps"
 sudo cp -R /var/lib/snapd/*  $BASEDIR/Backup/Snaps/
 echo "sudo cp -R /usr/share/applications/*.desktop  ./Backup/Applications/"
 sudo cp -R /usr/share/applications/*.desktop  $BASEDIR/Backup/Applications/
@@ -15,4 +17,3 @@ echo "sudo cp -R /etc/apt/sources.list.d/*  .R/Backup/sources/"
 sudo cp -R /etc/apt/sources.list.d/*  $BASEDIR/Backup/sources/
 echo "--------------------------------------------------------------------"
 echo "Backup done. This window should close automatically"
-fi

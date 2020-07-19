50163a7c1026995ca59ff1711beff925287ea014
ibuntuos <68012153+ibuntuos@users.noreply.github.com>
2020-07-19 11:23:33 +0200

    fix dependencies
    perform update of sources before trying to install dependencies

--- dependencies.sh
+++ dependencies.sh
@@ -3,5 +3,6 @@ echo "iBuntu Update Tool could not load we try to install missing dependencies"
 echo "After this window closes just open the app again."
 echo "========================================================================"
 echo "Installing: python3-tk, python3-pip and PySimpleGui:"
+sudo apt update
 sudo apt install python3-tk python3-pip -y
 pip3 install pysimplegui
